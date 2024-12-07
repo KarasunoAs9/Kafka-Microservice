fcac6fc76b364b645b5062d3c87083f50b118e1c
Dem <demagandrej@gmail.com>
2024-12-07 19:19:03 +0200

    add func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KarasunoAs9/microservice-go
+
+go 1.23.3
--- microservice/producer/cmd/main.go
+++ microservice/producer/cmd/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+
+type Order struct {
+	CustomerName string `json:"customer_name"`
+	CoffeType string `json:"coffe_type"`
+}
+
+
+func main() {
+	http.HandleFunc("/order", placeOrder)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}	
+
+func placeOrder(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+
+	
+}
