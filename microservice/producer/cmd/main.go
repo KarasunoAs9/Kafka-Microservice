374f483074e2b108987869ea4f9e2a43363ebc5f
Dem <demagandrej@gmail.com>
2024-12-07 19:40:44 +0200

    add producer logic

--- microservice/producer/cmd/main.go
+++ microservice/producer/cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/IBM/sarama"
 )
 
 
@@ -12,6 +16,39 @@ type Order struct {
 	CoffeType string `json:"coffe_type"`
 }
 
+func ConnectProducer(broker []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	return sarama.NewSyncProducer(broker, config)
+}
+
+func PushOrderToQueue(topic string, message []byte) error {
+	broker := []string {"localhost:9092"}
+
+	producer, err := ConnectProducer(broker)
+	if err != nil {
+		return err
+	}
+
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Order is stored in topic(%s)/partition(%d)/offset(%d)", topic, partition, offset)
+	
+	return nil
+}
 
 func main() {
 	http.HandleFunc("/order", placeOrder)
@@ -24,6 +61,23 @@ func placeOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	order := new(Order)
 
-	
+	if err := json.NewDecoder(r.Body).Decode(order); err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	orderInBytes, err := json.Marshal(order)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err  = PushOrderToQueue("coffer_order", orderInBytes)
+	if err != nil {
+		fmt.Println("error with push order to kafka:", err)
+	}
 }
